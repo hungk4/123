85af118bb4b694c368a127059288b1f1f33a8537
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-09-30 22:30:31 +0700

    ket noi database

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectDatabase } from "./config/connectDB";
+import Article from "./models/article.model";
+connectDatabase();
+
 const app: Express = express();
 const port: number = 3000;
 // Rest API
-app.get("/articles", (req: Request, res: Response) => {
+app.get("/articles", async (req: Request, res: Response) => {
+  const articles = await Article.find({
+    deleted: false
+  });
   res.json({
-    articles: []
+    articles: articles
   });
 });
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
