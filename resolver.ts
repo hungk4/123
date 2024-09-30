8e805dff4cf4680a0ed428bdd3e129cdcc7534d5
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-09-30 23:08:49 +0700

    lay ra danhh sach bai viet

--- resolver.ts
+++ resolver.ts
@@ -1,9 +1,14 @@
 import { Query } from "mongoose";
+import Article from "./models/article.model";
 
 export const resolvers = {
   Query: {
-    hello: () => {
-      return "hello world";
+    getListArticle: async () => {
+      const articles = await Article.find({
+        deleted: false
+      });
+      
+      return articles;
     }
   }
 };
